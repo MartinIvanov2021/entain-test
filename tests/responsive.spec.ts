96bc6a8be89a48f241d6faddcf20ae8a489583f1
MartinIvanov2021 <martin.iiivanov@gmail.com>
2025-06-01 15:23:04 +0300

    Update_responsive_test

--- tests/responsive.spec.ts
+++ tests/responsive.spec.ts
@@ -1,18 +1,28 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
 // Test: Ensure the Live Betting page is responsive.
 
 test.describe('Responsive Design Test', () => {
-  test('Desktop viewport', async ({ page }) => {
+  test('Desktop viewport', async ({ page,isMobile }) => {
     await page.goto('/');
-    await expect(page.locator('#header')).toBeVisible();
+
+    if (isMobile){
+        // Click on live betting
+      const live_bet_click: Locator =  page.locator('#sports-nav vn-menu-item');
+      await live_bet_click.first().click();
+      const confirm_data_column_mobile = page.locator('ms-live-overview');
+      await expect(confirm_data_column_mobile).toBeInViewport();
+    }
+    else {
+      const open_livebet: Locator = page.getByRole('link', { name: 'Live Betting', exact: true });
+      await open_livebet.first().click();
+
+      const confirm_data_column = page.locator('ms-main-column');
+      await expect(confirm_data_column).toBeInViewport();
+      //await expect(test1).toBeVisible()
+      //await expect(page.locator('#header')).toBeVisible();
+    }
   });
 
-  test.use({ viewport: { width: 375, height: 812 } }); // Mobile
-
-  test('Mobile viewport', async ({ page }) => {
-    await page.goto('/');
-    const mobileHeader = page.locator('[data-testid="mobile-header"]');
-    await expect(mobileHeader).toBeVisible();
-  });
+ 
 });
