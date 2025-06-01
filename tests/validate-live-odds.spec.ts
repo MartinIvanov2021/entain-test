ab01b916e2d371ac213225b17350e79e67e91164
MartinIvanov2021 <martin.iiivanov@gmail.com>
2025-06-01 17:13:55 +0300

    update_test_validate-live-odds

--- tests/validate-live-odds.spec.ts
+++ tests/validate-live-odds.spec.ts
@@ -1,20 +1,26 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 import { selectors } from '../utils/selectors';
 import { waitForOddsChange } from '../utils/helpers';
 
 // Test: Verify that live odds update without a page refresh and display change indicators.
 
-test('Validate Live Odds Update', async ({ page }) => {
+test('Validate Live Odds Update', async ({ page, }) => {
+
+
+
   await page.goto('/');
 
-  const pick = page.locator(selectors.firstPick).first();
+       const test: Locator = page.getByRole('link', { name: 'Live Betting', exact: true });
+       await test.first().click();
 
-  // Wait for odds to change using custom helper
-  const { initial, updated } = await waitForOddsChange(pick, 15000);
+      const open_tenis: Locator = page.getByRole('tab', { name: '56' }).locator('a');
+      await open_tenis.first().click();
 
-  expect(initial).not.toEqual(updated); // Validate odds have changed
+      const add_bet_slip = page.locator('.option-indicator').first();
+      const odds_value = await add_bet_slip.allInnerTexts();
+      
+      await expect(add_bet_slip).not.toHaveText(odds_value,{timeout: 10000});
+    
 
-  // Verify UI change indicator (e.g., red/green arrow or flash)
-  const indicator = pick.locator(selectors.oddsChangeIndicator);
-  await expect(indicator).toBeVisible();
+
 });
