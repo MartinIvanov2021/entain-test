8962e2b7013452343d63a4e3282c9dd0e161900b
MartinIvanov2021 <martin.iiivanov@gmail.com>
2025-06-01 14:41:24 +0300

    Update_betslip_test

--- tests/add-pick-to-betslip.spec.ts
+++ tests/add-pick-to-betslip.spec.ts
@@ -1,19 +1,41 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 import { selectors } from '../utils/selectors';
 
 // Test: Add a live pick to the betslip and validate it appears correctly.
 
-test('Add pick to Betslip', async ({ page }) => {
+test('Add pick to Betslip', async ({ page, isMobile }) => {
   await page.goto('/');
   await page.waitForLoadState('domcontentloaded');
 
-  const firstPick = page.locator(selectors.firstPick);
-  await firstPick.first().click(); // Add first available pick
+  // Go to live betting 
 
-  // Verify pick appears in the betslip
-  const betslipItem = page.locator(selectors.betslipItem);
-  await expect(betslipItem).toBeVisible();
+  if (isMobile){
+    // Click on live betting
+    const live_bet_click: Locator =  page.locator('#sports-nav vn-menu-item');
+    await live_bet_click.first().click();
 
-  // Confirm pick is highlighted (active class)
-  await expect(firstPick.first()).toHaveClass(/selected/);
+    // add bet slip 
+    const add_bet_slip = page.locator('.option-indicator');
+    await add_bet_slip.first().click();
+
+    // verify pick appears in the betslip 
+    const betslip_bar = page.locator('bs-betslip-bar');
+    await expect(betslip_bar).toBeVisible();
+
+  }else {
+    const test: Locator = page.getByRole('link', { name: 'Live Betting', exact: true });
+    await test.first().click(); // Add first available pick
+  
+    // verify if the bet is added in betslip
+  
+    const add_bet_slip = page.locator('.option-indicator');
+    await add_bet_slip.first().click();
+   
+  
+    // Verify pick appears in the betslip
+    const betslipItem = page.locator('bs-digital-single-bet-pick');
+    await expect(betslipItem).toBeVisible();
+  }
+
+
 });
